76b98febfbe1ba9ea1f98e664f4260715c0c8065
Rohitvishwakarma97 <86231128+Rohitvishwakarma97@users.noreply.github.com>
2021-06-24 12:00:01 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,7 +3,24 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    if(n==0)
+    {
+        return  0;
+    }
+          
+    String bin="";
+    
+    While (n>0)
+    {
+          bin=((n&1)==0?'0':'1')+bin;
+          n>>=1;
+        
+    }
+    return "bin";
   }
 }
+public static void main(String args[])
+{
+    int n=35 ;
+    System.out.println(toBinary(n));
+ }
